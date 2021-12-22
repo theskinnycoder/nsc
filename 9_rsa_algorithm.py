f267a438937c511e35ae82ecffcb09116128da27
theskinnycoder <18131a0505@gvpce.ac.in>
2021-12-23 02:15:59 +0530

    changed rsa

--- 9_rsa_algorithm.py
+++ 9_rsa_algorithm.py
@@ -1,33 +1,9 @@
-import math
-
-
-# Extended Euclid's Algorithm for Modular Multiplicative Inverse
-def euclidean_mod_inverse(a, b):
-    temp = b
-
-    # Initialize variables
-    t1, t2 = 0, 1
-
-    if b == 1:
-        return 0
-
-    # Perform extended Euclid's algorithm until a > 1
-    while a > 1:
-        quotient, remainder = divmod(a, b)
-        a, b = b, remainder
-
-        t1, t2 = t2 - t1 * quotient, t1
-
-    if (t2 < 0) :
-        t2 += temp
-
-    return t2
-
+from math import gcd
 
 # Select Public Key
 def get_public_key(phi):
     for i in range(2, phi):
-        if math.gcd(i, phi) == 1:
+        if gcd(i, phi) == 1:
             return i
 
 # RSA Encryption Algorithm
@@ -42,7 +18,7 @@ def decrypt(encrypted_msg, d, n):
 
 # Driver Code
 if __name__ == '__main__':
-    plain_msg = 89
+    plain_msg = 12
     p, q = 3, 7
 
     n = p * q
